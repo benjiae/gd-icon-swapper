3c3930ceb59c5d06865d692cc81213c7d6ffdfbb
benjae <benjaminang2009@gmail.com>
2024-03-08 16:40:10 -0300

    ported the thing to geode textureldr

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import shutil
 
 path = "icons/"
-path_backup = path + "backups/"
+ldr_path = "replaced_icons/icons/"
 
-if not os.path.isdir(path):
+if not os.path.isdir("icons"):
     print("No icons folder found, please copy your icons folder to this directory (Geometry Dash/Resources/icons)")
     exit
 
-if not os.path.isdir(path_backup):
-    print("no backups folder, creating one")
-    os.mkdir(path_backup)
+if not os.path.isdir("replaced_icons"):
+    print("no pack folder, creating one")
+    os.mkdir("replaced_icons")
+
+if not os.path.isdir("replaced_icons/icons"):
+    print("no pack icons folder, creating one")
+    os.mkdir("replaced_icons/icons")
+    
 
 while True:
     print("Select the gamemode you want:")
@@ -54,7 +59,7 @@ while True:
             replaced_id = int(input("Insert the ID of the icon you want to replace: "))
             break
         except ValueError:
-            print("Please, insert an integer.")
+            print("Please, insert an integer number.")
             continue
 
     print("")
@@ -89,60 +94,25 @@ while True:
         case 9:
             mode_string = "jetpack_"
 
-    print("Moving original files to backups folder...")
-    ### Moving the original files to the backups folder
-    os.rename(path + mode_string + replaced + ".png", path_backup + mode_string + replaced + ".png")
-    os.rename(path + mode_string + replaced + ".plist", path_backup + mode_string + replaced + ".plist")
+    list_ = [".png", "-hd.png", "-uhd.png", ".plist", "-hd.plist", "-uhd.plist"]
 
-    os.rename(path + mode_string + replaced + "-hd.png", path_backup + mode_string + replaced + "-hd.png")
-    os.rename(path + mode_string + replaced + "-hd.plist", path_backup + mode_string + replaced + "-hd.plist")
-
-    os.rename(path + mode_string + replaced + "-uhd.png", path_backup + mode_string + replaced + "-uhd.png")
-    os.rename(path + mode_string + replaced + "-uhd.plist", path_backup + mode_string + replaced + "-uhd.plist")
-
-    print("Replacing original files with the other icon's ones...")
-    ### Copying the replacement files
-
-    shutil.copyfile(path + mode_string + replacing + ".png", path + mode_string + replaced + ".png")
-    shutil.copyfile(path + mode_string + replacing + ".plist", path + mode_string + replaced + ".plist")
-
-    shutil.copyfile(path + mode_string + replacing + "-hd.png", path + mode_string + replaced + "-hd.png")
-    shutil.copyfile(path + mode_string + replacing + "-hd.plist", path + mode_string + replaced + "-hd.plist")
-
-    shutil.copyfile(path + mode_string + replacing + "-uhd.png", path + mode_string + replaced + "-uhd.png")
-    shutil.copyfile(path + mode_string + replacing + "-uhd.plist", path + mode_string + replaced + "-uhd.plist")
+    for quality in list_:
+        shutil.copyfile(path + mode_string + replacing + quality, ldr_path + mode_string + replaced + quality)
 
     print("Editing the plist files...")
     ### Editing the plist files
 
-    ## low quality
-    with open(path + mode_string + replaced + ".plist", 'r') as file:
-        filedata = file.read()
+    plists = [".plist", "-hd.plist", "-uhd.plist"]
 
-        filedata = filedata.replace(mode_string + replacing, mode_string + replaced)
-
-    with open(path + mode_string + replaced + ".plist", 'w') as file:
-        file.write(filedata)
-
-    ## medium quality
-    with open(path + mode_string + replaced + "-hd.plist", 'r') as file:
-        filedata = file.read()
-
-        filedata = filedata.replace(mode_string + replacing, mode_string + replaced)
-
-    with open(path + mode_string + replaced + "-hd.plist", 'w') as file:
-        file.write(filedata)
-
-    ## high quality
-    with open(path + mode_string + replaced + "-uhd.plist", 'r') as file:
-        filedata = file.read()
-
-        filedata = filedata.replace(mode_string + replacing, mode_string + replaced)
-
-    with open(path + mode_string + replaced + "-uhd.plist", 'w') as file:
-        file.write(filedata)
+    for plist in plists:
+        with open(path + mode_string + replacing + plist, "r") as file:
+            filedata = file.read()
+            filedata = filedata.replace(mode_string + replacing, mode_string + replaced)
+        with open(ldr_path + mode_string + replaced + plist, "w") as file:
+            file.write(filedata)
 
     print("")
+    shutil.copyfile("pack.json","replaced_icons/pack.json")
     while True:
         try:
             repeat = int(input("Done! Do you want to make another change? (1 for Yes, 0 for No): "))
@@ -156,4 +126,4 @@ while True:
     elif repeat == 0:
         break
 print("")
-print("Copy the icons folder from this directory to the Resources folder.")
+print("Copy the icons folder from this directory to the TextureLDR packs folder.")
